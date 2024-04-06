da395d6c7acb7f212a28f608710a6ec48b8345b4
Imri <imri.harlev@gmail.com>
2024-04-06 13:13:56 +0300

    fix md file

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#Welcome to my Appointment Schedule App!
-##This app was built to show my self-learning skills
-creator - Imri Harlev
-contacts - imri.harlev@gmail.com
+# Welcome to my Appointment Schedule App!
+## This app was built to show my self-learning skills
+*creator* - Imri Harlev<br />
+*contacts* - imri.harlev@gmail.com<br />
--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { router, useRootNavigationState } from "expo-router";
+import { Redirect, router, useRootNavigationState } from "expo-router";
 import { atom, useAtomValue } from "jotai";
 import { User } from "../src/models/user.model";
 import { I18nManager, View } from "react-native";
@@ -9,26 +9,17 @@ export const userAtom = atom<User | undefined>(undefined);
 const App = () => {
   const user = useAtomValue(userAtom);
   registerTranslation("he", he);
-  
-  const shouldBeRTL = true;
-  if (shouldBeRTL !== I18nManager.isRTL) {
-    I18nManager.allowRTL(shouldBeRTL);
-    I18nManager.forceRTL(shouldBeRTL);
+  //const shouldBeRTL = true;
+  // if (shouldBeRTL !== I18nManager.isRTL) {
+  //   I18nManager.allowRTL(shouldBeRTL);
+  //   I18nManager.forceRTL(shouldBeRTL);
+  // }
+  if (user) {
+    return <Redirect href="/appointment" />;
+  } else {
+    return <Redirect href="/login" />;
   }
-  
-  const rootNavigationState = useRootNavigationState();
-  const navigatorReady = rootNavigationState?.key != null;
-  
-  useEffect(() => {
-    if (!navigatorReady) return;
-    if (user) {
-      router.push("/appointment");
-    } else {
-      router.push("/login");
-    }
-  }, [navigatorReady]);
 
-  return <View></View>;
 };
 
 export default App;
